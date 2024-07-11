9fc525c839ebde6affb17110723088071c7ff184
Loric <loricsstudios@gmail.com>
2024-07-11 20:51:34 +0200

    many updates, now configurable through api endpoint

--- plotter_server.py
+++ plotter_server.py
@@ -1,12 +1,11 @@
+# import pprint
 import threading
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from fastapi import FastAPI
-# , HTTPException, Request
 from pydantic import BaseModel, Field
 from typing import Any, Dict
 import uvicorn
-# import json
 
 app = FastAPI()
 
@@ -17,76 +16,70 @@ app = FastAPI()
 
 # Initial Configuration
 CONFIG = {
-    'plot_title': 'Graph x',
+    'plot_title': 'A graph',
     'x_label': 'Time',
-    'y_label': 'Values',
+    'y_label': 'Value',
     # 'x_range': (0, 100),
     'y_range': (0, 100),
     'data_sets': {
         'set1': {'color': 'r', 'label': 'Target security', 'marker': 'o'},
         'set2': {'color': 'b', 'label': 'Target money', 'marker': 's'},
-        'set3': {'color': 'g', 'label': 'Overall money', 'marker': '^'},
+        'set3': {'color': 'g', 'label': 'Overall money', 'marker': ','},
     },
     'legend_location': 'upper right',
     'max_points': 180,
 }
 
-
-
-
-
-
-
 all_plots = {
-    '1':{
-        'data_sets':  {set_name: {'x': [], 'y': []} for set_name in CONFIG['data_sets']},
+    1:{
+        'data_sets':  {
+                # 'empty': {'x': [], 'y': [], 'anot': None} 
+            },
         'fig': None,
         'ax': None,
         'lines': {},
         'TTL': None, 
     },
-    '2':{
-        'data_sets':  {set_name: {'x': [], 'y': []} for set_name in CONFIG['data_sets']},
-        'fig': None,
-        'ax': None,
-        'lines': {},
-        'TTL': None, 
-    }
-} #TBD
+    # '2':{
+    #     'data_sets':  {set_name: {'x': [], 'y': []} for set_name in CONFIG['data_sets']},
+    #     'fig': None,
+    #     'ax': None,
+    #     'lines': {},
+    #     'TTL': None, 
+    # }
+}
 
-all_plots['1']['fig'], all_plots['1']['ax'] = plt.subplots()
-all_plots['2']['fig'], all_plots['2']['ax'] = plt.subplots()
+all_plots[1]['fig'], all_plots[1]['ax'] = plt.subplots()
+# all_plots['2']['fig'], all_plots['2']['ax'] = plt.subplots()
 
-def update_plot_config(plot_id):
+def update_plot_config(plot_id, data):
+    global all_plots
     ax = all_plots[plot_id]['ax']
     ax.clear()
-    ax.set_title(CONFIG['plot_title'])
-    ax.set_xlabel(CONFIG['x_label'])
-    ax.set_ylabel(CONFIG['y_label'])
-    # ax.set_xlim(CONFIG['x_range'])
-    ax.set_ylim(CONFIG['y_range'])
-  
-    all_plots[plot_id]['lines'] = {set_name: ax.plot([], [], 
-                               f"{CONFIG['data_sets'][set_name]['color']}{CONFIG['data_sets'][set_name]['marker']}-", 
-                               label=CONFIG['data_sets'][set_name]['label'])[0] 
-             for set_name in CONFIG['data_sets']}
-
-    ax.legend(loc=CONFIG['legend_location'])
-
-CONFIG['plot_title'] = "apples"
-update_plot_config('1')
-CONFIG['plot_title'] = "bananas"
-update_plot_config('2')
+    ax.set_title(data.plot_title ) # mandatory
+    ax.set_xlabel(data.x_label)
+    ax.set_ylabel(data.y_label)
+    # ax.set_xlim(CONFIG['x_range']) # this is set dynamically
+    ax.set_ylim(data.y_range)
+    # mandatory, no defaults
+    all_plots[plot_id]['lines'] = {}
+    for k,v in data.data_sets.items():
+        all_plots[plot_id]['lines'][k] = ax.plot([], [], color=v.color, marker= v.marker, label=v.label)[0]
+        anot = ax.annotate(k, xy=(0, 0), xytext=(5, 0), textcoords='offset points', ha='left', va='center', color=v.color) 
+        all_plots[plot_id]['data_sets'][k] = {'x': [], 'y': [], 'anot': anot}
+    ax.legend(loc=data.legend_location, prop={'size': 6})
 
 class DataPoint(BaseModel):
-    # id: str
     x: float
     y: float
 
 
+# @app.delete("/id/{plot_id}")
+# async def despawn_plot(plot_id: str):
+#     return {"status" : f"hello world! plot id to be deleted: {plot_id}"}
 
 @app.post("/id/{plot_id}/set/{set_name}") # U/CRUD
-async def receive_data(plot_id: str, set_name: str, data: DataPoint):
+async def receive_data(plot_id: int, set_name: str, data: DataPoint):
     if plot_id not in all_plots.keys():
         return {"status": "Attempting to send data to nonexistant plot, spawn it first via /config endpoint!"}
     if set_name not in all_plots[plot_id]['data_sets']:
@@ -117,26 +110,17 @@ class DataFrame(BaseModel):
     class Config:
         extra = "allow" 
 
-@app.post("/config") # CU/CRUD
-async def update_config(data: DataFrame):
-    return {"status":"Config received", "plot_title": data.plot_title}
-    # try:
-    #     parsed_data = json.loads(data)
-    #     for key, value in parsed_data.items():
-    #         if key in CONFIG:
-    #             CONFIG[key] = value
-    #     update_plot_config()
-    #     return {"status": "Configuration updated", "config": CONFIG}
-    # except json.JSONDecodeError:
-    #     return {"status": "Error", "message": "Invalid JSON"}
+@app.post("/config/{plot_id}") # CU/CRUD
+async def update_config(plot_id: int, data: DataFrame):
+    update_plot_config(plot_id, data)
+    return {"status":"Config received and updated", "plot_id": plot_id, "plot_title": data.plot_title}
     
-
 def update_plot(frame): # tried to have it parametrizable but no bueno :/
     # dynamic X range (in time), do it separately for each plot based on it's datasets
     ret = []
+    global all_plots
 
     for plot in all_plots.values():    
-        # plot = all_plots['1'] # try redo this as a method of object so it can still be callable without param?
         data_sets = plot['data_sets']
 
         all_x = [x for set_data in data_sets.values() for x in set_data['x']]
@@ -147,23 +131,29 @@ def update_plot(frame): # tried to have it parametrizable but no bueno :/
         for set_name, line in plot['lines'].items():
             line.set_data(data_sets[set_name]['x'], data_sets[set_name]['y'])
             ret.append(line)
+        for v in data_sets.values(): # highly experimental /debug
+            # print(v['anot'])
+            v['anot'].set_position((v['x'][-1], v['y'][-1]))
+            ret.append(v['anot'])
+
         
         plot['ax'].relim()
-        plot['ax'].autoscale_view(scalex=False, scaley=True)  # Only autoscale y-axis
+        plot['ax'].autoscale_view(scalex=True, scaley=True)  # Only autoscale y-axis
     return ret
-# return must be a tuple or list of ax.plot objects
-#tuple(plot['lines'].values())
+
+    # for i, (x, y) in enumerate(samples): #maybe belongs here? trying to annotate axes
+    # plt.plot(x, y)
+    # plt.text(x[-1], y[-1], f'sample {i}')
+# return must be a tuple or list of ax.plot and similar ax. objects
+
 
 def run_server():
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
 
 def run_plot():
     anims = []
-    for plot_k, plot_v in all_plots.items():
+    for plot_v in all_plots.values():
         anims.append(FuncAnimation(plot_v['fig'], update_plot, interval=100, blit=False))
-    
-    # anims = (FuncAnimation(plot_v['fig'], update_plot, interval=100, blit=True))
-    print(f'num of anims: {len(anims)}')
     plt.show()
 
 if __name__ == "__main__":
